71552e4b998d45c27d25ee5740f1a0e06b5bb15f
GoEun <w2219@e-mirim.hs.kr>
2023-06-12 12:19:25 +0900

    php_study

--- test1.php
+++ test1.php
@@ -1,5 +1,15 @@
 <?php
-
-for()
-
+$a = 0;
+$b = 0;
+$c = 0;
+  for($a;$a<=100;$a++){
+    if($a%2==0){
+    $b += $a;
+    }
+  else{
+    $c += $a;
+  }
+  }
+  echo $_GET['num']."<br>";
+  echo $_GET['data2'];
 ?>
